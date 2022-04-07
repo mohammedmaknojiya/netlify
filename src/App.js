6fbb008b964dfc3d222c5b6d7243a689b33bcd21
mohammedmaknojiya <mmra9593@gmail.com>
2022-04-07 23:25:26 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Mohammed Maknojiya
+          Learn React with Mohammed Maknojiya !!
         </a>
       </header>
     </div>
